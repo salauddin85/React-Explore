7ed252d063ba13a64eb79a47bd7119a9418a7df0
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-04-29 23:22:01 +0600

    m6 pricing options project setup done

---
+++ pricing_options_M6/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500 cursor-pointer">
+        Vite + React
+      </h1>
+      <button class="btn btn-primary">Button</button>
+      <details className="dropdown">
+        <summary className="btn m-1">open or close</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </details>
+      <div role="alert" className="alert alert-success">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  <span>Your purchase has been confirmed!</span>
+</div>
+    </>
+  );
+}
+
+export default App;
