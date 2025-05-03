c98bdee9482e9fd2a7ccc741b7270bc197ed6857
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-03 20:57:41 +0600

    firest router  added in react

--- react-router-explore-M7/src/main.jsx
+++ react-router-explore-M7/src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+  {
+
+    path: "/contact",
+    element: <div>Contact</div>,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
