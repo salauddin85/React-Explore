65db26257f46b9941694d7c60eb68d3e01f01c56
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-05 23:10:08 +0600

    change email classname

--- react-router-explore-M7/src/Pages/Users/User/User.jsx
+++ react-router-explore-M7/src/Pages/Users/User/User.jsx
@@ -5,10 +5,10 @@ function User({ user }) {
   // console.log(user);
   const { id, name, email } = user;
   return (
-    <div className="border-2 border-red-500 p-5 rounded-2xl " >
+    <div className="border-2 border-red-500 p-5 rounded-2xl  " >
       <h1>User Id: {id}</h1>
       <h4>Name: {name}</h4>
-      <h4>Email: {email}</h4>
+      <h4 className="mt-3 text-gray-500 font-bold  ">Email: {email}</h4>
       <Link to={`/users/${id}`}> <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-3" >Details</button>
       </Link>
     </div>
