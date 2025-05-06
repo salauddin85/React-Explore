b30b231fbdecf4473adaa1cb47a640b0bc7a912c
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-06 23:32:40 +0600

    added loding spinner and use navLink

--- react-router-explore-M7/src/Pages/Headar/Headar.jsx
+++ react-router-explore-M7/src/Pages/Headar/Headar.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import './Headar.css'
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 function Headar() {
     return (
         <div>
             <nav>
                 <ul  >
                
-                    <Link to ="/">Home</Link>
+                    {/* <Link to ="/">Home</Link>
                     <Link to ="/about">About</Link>
                     <Link to ="/contact">Contact</Link>
                     <Link to ="/users">Users</Link>
-                    <Link to="/posts">Posts</Link>
+                    <Link to="/posts">Posts</Link> */}
+
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/users">Users</NavLink>
+                    <NavLink to="/posts">Posts</NavLink>
+                    
                 </ul>
             </nav>
         </div>
--- react-router-explore-M7/src/Pages/Home/Home.jsx
+++ react-router-explore-M7/src/Pages/Home/Home.jsx
@@ -1,16 +1,25 @@
-import {React} from 'react';
-import { Outlet } from 'react-router-dom';
+import { React } from 'react';
+import { Outlet, useNavigation, useLocation } from 'react-router-dom';
 import Header from '../Headar/Headar';
 import Footer from '../Footer/Footer';
 
-
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div>
-            <h1>Hello world from Home page</h1>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
+            <Header />
+            {
+                navigation.state === 'loading' && (
+                    <div className="flex justify-center items-center my-10">
+                        <div className="w-50 h-50 border-4 border-blue-500 border-dashed rounded-full animate-spin "></div>
+                    </div>
+                )
+            }
+            <Outlet />
+            <Footer />
         </div>
     );
 };
