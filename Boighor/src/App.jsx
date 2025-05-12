f91b1c45f1aad6838a8abb819e58dd83211a62a7
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-12 23:14:55 +0600

    added project setup fo Boighor

---
+++ Boighor/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button className="btn btn-neutral">Neutral</button>
+
+    </>
+  )
+}
+
+export default App
---
+++ Boighor/src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h2>Hello world from root</h2>
+        </div>
+    );
+};
+
+export default Root;
