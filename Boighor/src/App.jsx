f18ad3fb24d71afb16773a75dd09eb3d18f76f16
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-13 23:06:00 +0600

    updated app.jsx

--- Boighor/src/App.jsx
+++ Boighor/src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <p>Click the button below to see the effect:</p>
+      <button className="btn btn-primary">Primary</button>
+
+
       <button className="btn btn-neutral">Neutral</button>
+
 
     </>
   )
