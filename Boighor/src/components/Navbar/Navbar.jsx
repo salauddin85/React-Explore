1951610da88cb1760ffcce20d6485d0e36e4ffe4
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-17 22:29:45 +0600

    added Nablink

--- Boighor/src/components/Navbar/Navbar.jsx
+++ Boighor/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-//  common componentfunction for button,pages to read,listed books
- const commonComponent = (text) => {
-    return text
-};
+// //  common componentfunction for button,pages to read,listed books
+//  const commonComponent = (text) => {
+//     return text
+// };
+  const links = <>
+    <li className="font-bold  text-black"><NavLink  to="/">Home</NavLink></li>
+    <li className="font-bold text-black"><NavLink to="/books">Books</NavLink></li>
+    <li className="font-bold text-black"><NavLink to="/listedBooks">Listed Books</NavLink></li>
+    <li className="font-bold text-black"><NavLink to="/pagesToRead">Pages To Read</NavLink></li>
+  </>
 
   return (
    <div class="navbar w-full bg-white shadow-sm px-0 md:px-4 lg:px-10">
@@ -33,34 +40,18 @@ const Navbar = () => {
           </div>
           <ul
             tabindex="0"
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            class="menu menu-sm dropdown-content bg-base-100  rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <p className="text-black font-bold text-lg">{commonComponent("Home")}</p>
-            </li>
-            <li>
-              <p className="text-black font-bold text-lg">{commonComponent("Listed Books")}</p>
-            </li>
-            <li>
-              <p className="text-black font-bold text-lg">{commonComponent("Pages to Read")}</p>
-            </li>
+           
+            {links}
           </ul>
         </div>
         <a class="  normal-case font-bold   lg:text-2xl  text-green-600">BOIGHOR</a>
       </div>
       <div class="navbar hidden lg:flex">
-        <ul class="menu menu-horizontal px-1 gap-3">
-          <li>
-            <Link to="/">
-              <button className="text-black font-bold text-lg">{commonComponent("Home")}</button>
-            </Link>
-          </li>
-          <li>
-            <p className="text-black font-bold text-lg">{commonComponent("Listed Books")}</p>
-          </li>
-          <li>
-            <p className="text-black font-bold text-lg">{commonComponent("Pages to Read")}</p>
-          </li>
+        <ul class="menu menu-horizontal px-1 ms-[100px] gap-3 ">
+         
+          {links}
         </ul>
       </div>
       <div class="navbar-end  gap-3 lg:flex">
