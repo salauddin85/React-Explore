455434ab14850a1e4232621e0d4b91d09ff8e293
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-22 23:15:45 +0600

    google sign out added by firebase

--- react_authentication_M9/src/components/Login/Login.jsx
+++ react_authentication_M9/src/components/Login/Login.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 // import { SignInWithPopup } from 'firebase/auth';
 import auth from '../../firebase/firebase.js';
+import { useState } from 'react';
 const Login = () => {
     const provider = new GoogleAuthProvider();
+    const [user, setUser] = useState(null);
     const handleGoogleSignIn = () => {
         console.log('Google Sign In');
         // Sign in logic here
@@ -14,9 +16,11 @@ const Login = () => {
                 const token = credential.accessToken;
                 // The signed-in user info.
                 const user = result.user;
+                setUser(user);
                 console.log(user);
             }).catch((error) => {
                 // Handle Errors here.
+                setUser(null);
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 // The email of the user's account used.
@@ -25,9 +29,28 @@ const Login = () => {
                 const credential = GoogleAuthProvider.credentialFromError(error);
             });
     }
+    const handleSignOut = () => {
+        auth.signOut()
+            .then(() => {
+                // Sign-out successful.
+                console.log('Sign-out successful.');
+                setUser(null);
+            }).catch((error) => {
+                // An error happened.
+                console.log('An error happened.', error);
+            });
+    }
     return (
         <div>
             <button className='bg-sky-400 m-5 px-5 py-3 rounded border border-amber-950' onClick={handleGoogleSignIn} >Login</button>
+            <button className='bg-red-400 m-5 px-5 py-3 rounded border border-amber-950' onClick={handleSignOut} >Logout</button>
+
+            {user && (
+                <div>
+                    <h1 className='text-3xl font-bold'>Welcome, {user.displayName}</h1>
+                    <img src={user.photoURL} alt={user.displayName} />
+                </div>
+            )}
         </div>
     );
 };
