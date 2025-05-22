764783257d1ce9db5793b89c22b6b2ec69e846ed
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-22 22:43:28 +0600

    added firebase authentication

--- react_authentication_M9/src/components/Login/Login.jsx
+++ react_authentication_M9/src/components/Login/Login.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { GoogleAuthProvider } from 'firebase/auth/web-extension';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+// import { SignInWithPopup } from 'firebase/auth';
+import auth from '../../firebase/firebase.js';
 const Login = () => {
     const provider = new GoogleAuthProvider();
     const handleGoogleSignIn = () => {
         console.log('Google Sign In');
+        // Sign in logic here
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user);
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            });
     }
     return (
         <div>
