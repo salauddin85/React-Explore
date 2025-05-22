0078ef84a4b18bb8e4409ab24df802c46b27cc5d
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-22 23:49:15 +0600

    added login by github

--- react_authentication_M9/src/components/Login/Login.jsx
+++ react_authentication_M9/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup , GithubAuthProvider} from 'firebase/auth';
 // import { SignInWithPopup } from 'firebase/auth';
 import auth from '../../firebase/firebase.js';
 import { useState } from 'react';
@@ -29,6 +29,30 @@ const Login = () => {
                 const credential = GoogleAuthProvider.credentialFromError(error);
             });
     }
+    const handleGithubSignIn = () => {
+        console.log('Github Sign In');
+        // Sign in logic here
+        const provider = new GithubAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Github Access Token. You can use it to access the Github API.
+                const credential = GithubAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            }).catch((error) => {
+                // Handle Errors here.
+                setUser(null);
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GithubAuthProvider.credentialFromError(error);
+            });
+    }
     const handleSignOut = () => {
         auth.signOut()
             .then(() => {
@@ -42,15 +66,22 @@ const Login = () => {
     }
     return (
         <div>
-            <button className='bg-sky-400 m-5 px-5 py-3 rounded border border-amber-950' onClick={handleGoogleSignIn} >Login</button>
-            <button className='bg-red-400 m-5 px-5 py-3 rounded border border-amber-950' onClick={handleSignOut} >Logout</button>
+            
 
             {user && (
+
                 <div>
                     <h1 className='text-3xl font-bold'>Welcome, {user.displayName}</h1>
                     <img src={user.photoURL} alt={user.displayName} />
                 </div>
             )}
+            { user ?
+            <button className='bg-red-400 m-5 px-5 py-3 rounded border border-amber-950' onClick={handleSignOut} >Logout</button> :
+        <>
+                    <button className='bg-sky-400 m-5 px-5 py-3 rounded border border-amber-950' onClick={handleGoogleSignIn} >Login</button>
+                    <button className='bg-red-400 m-5 px-5 py-3 rounded border border-amber-950' onClick={handleGithubSignIn} >Github</button>
+        </>            
+}
         </div>
     );
 };
